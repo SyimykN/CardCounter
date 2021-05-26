b8bc7d9d34fad9339a63595edc30becd03471252
iosDevkg <syimykdev@gmail.com>
2021-05-26 10:43:55 +0600

    add a tableview and footer for him

---
+++ CardCounter/CellFooter/ResultTableViewFooter.swift
@@ -0,0 +1,44 @@
+//
+//  ResultTableViewFooter.swift
+//  CardCounter
+//
+//  Created by Sema on 5/26/21.
+//
+
+import UIKit
+
+class ResultTableViewFooter: UITableViewHeaderFooterView {
+    static let identifier  = "ResultTableViewFooter"
+    
+    let resultLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Theres not enough money in your cards!"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.layer.borderColor = UIColor.darkGray.cgColor
+        label.layer.borderWidth = 1
+        label.layer.cornerRadius = 4
+        return label
+    }()
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(resultLabel)
+        contentView.backgroundColor = .systemGray6
+        let constraints = [
+            resultLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            resultLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4),
+            resultLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -4),
+            resultLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            resultLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- CardCounter/ResultViewController.swift
+++ CardCounter/ResultViewController.swift
@@ -8,18 +8,54 @@
 import UIKit
 
 class ResultViewController: UIViewController {
+    //MARK: - Var and Lets
     var sum = 0
     var numOfCards = 0
     var arrayOfValueAndCount : [ValueCount] = []
-    
+    //MARK: - UI
+    let tableView : UITableView = {
+        let table = UITableView()
+//        table.register(MyTableViewCell.self, forCellReuseIdentifier: MyTableViewCell.identifier)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.register(ResultTableViewFooter.self, forHeaderFooterViewReuseIdentifier: "footer")
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.backgroundColor = .systemGray6
+        table.separatorStyle = .none
+        return table
+    }()
+    //MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .systemGray6
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
         title = "Result"
-        print("Sum is \(sum)")
-        print("numOfCards is \(numOfCards)")
-        print("arrayOfValueAndCount is \(arrayOfValueAndCount)")
+        
+        let constraints = [
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
     }
 }
-
+extension ResultViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Some text"
+        return cell
+    }
+    //footer
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footer = tableView.dequeueReusableHeaderFooterView(withIdentifier: "footer") as! ResultTableViewFooter
+//        footer.delegate = self
+        return footer
+    }
+}
